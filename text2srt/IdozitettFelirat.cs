6fd00751829c2c182f66b4b8af4939b822a853ff
mmiillmm <kmakai520@gmail.com>
2025-05-22 12:12:53 +0200

    TXT2STR-ElesVizsgaMegoldas

---
+++ text2srt/IdozitettFelirat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text2srt
+{
+    internal class IdozitettFelirat
+    {
+        public IdozitettFelirat(string idozites, string felirat)
+        {
+            Idozites = idozites;
+            Felirat = felirat;
+        }
+
+        public string Idozites { get; set; }
+        public string Felirat { get; set; }
+
+        public int SzavakSzama => Felirat.Split(' ').Length;
+
+        public string SrtIdozites
+        {
+            get
+            {
+                var parts = Idozites.Split(" - ");
+                return $"{ToSrtTime(parts[0])} --> {ToSrtTime(parts[1])}";
+            }
+        }
+
+        private static string ToSrtTime(string percMasodperc)
+        {
+            var pm = percMasodperc.Split(':');
+            int perc = int.Parse(pm[0]);
+            int mp = int.Parse(pm[1]);
+            int ora = perc / 60;
+            int percMaradek = perc % 60;
+            return $"{ora:00}:{percMaradek:00}:{mp:00}";
+        }
+    }
+}
---
+++ text2srt/Program.cs
@@ -0,0 +1,39 @@
+﻿using text2srt;
+
+List<IdozitettFelirat> idfel = new List<IdozitettFelirat>();
+
+using (StreamReader sr = new(@"..\..\..\src\feliratok.txt", System.Text.Encoding.UTF8))
+{
+    while (!sr.EndOfStream)
+    {
+        string? timing = sr.ReadLine();
+        if (string.IsNullOrWhiteSpace(timing)) break;
+
+        string? subtitle = sr.ReadLine();
+        if (string.IsNullOrWhiteSpace(subtitle)) break;
+
+        idfel.Add(new IdozitettFelirat(timing, subtitle));
+    }
+}
+
+Console.WriteLine($"5. feladat - Feliratok száma: {idfel.Count}");
+
+if (idfel.Count > 0)
+{
+    var maxFelirat = idfel.MaxBy(f => f.SzavakSzama);
+    Console.WriteLine("7. feladat - A legtöbb szóból álló felirat:");
+    Console.WriteLine(maxFelirat.Felirat);
+}
+
+using (StreamWriter sw = new(@"..\..\..\src\felirat.srt", false, System.Text.Encoding.UTF8))
+{
+    for (int i = 0; i < idfel.Count; i++)
+    {
+        sw.WriteLine(i + 1);
+        sw.WriteLine(idfel[i].SrtIdozites);
+        sw.WriteLine(idfel[i].Felirat);
+        sw.WriteLine();
+    }
+}
+
+Console.ReadKey();
