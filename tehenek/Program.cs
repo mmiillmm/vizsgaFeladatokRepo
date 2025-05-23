af214da8d6bbb3850d0333ce7a8bcd61fb5f4f39
mmiillmm <kmakai520@gmail.com>
2025-05-23 11:46:48 +0200

    HappyCowsCLI

---
+++ tehenek/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.CompilerServices;
+using tehenek;
+
+List<Tehen> happyCows = new List<Tehen>();
+
+using (StreamReader sr = new(@"..\..\..\src\hozam.txt", System.Text.Encoding.UTF8))
+{
+    while (!sr.EndOfStream)
+    {
+        string? line = sr.ReadLine();
+        string[] parts = line.Split(';');
+
+        string id = parts[0];
+        string nap = parts[1];
+        string menny = parts[2];
+
+        Tehen? aktTehen = happyCows.FirstOrDefault(t => t.Id == id);
+        if (aktTehen == null)
+        {
+            aktTehen = new Tehen(id);
+            happyCows.Add(aktTehen);
+        }
+
+        aktTehen.EredmenytRogzit(nap, menny);
+    }
+
+    Console.WriteLine($"3. Feladat:\n Az állomány {happyCows.Count} tehén adatait tartalmazza.");
+}
+
+var eligibleCows = happyCows.Where(t => t.Mennyisegek.Count(m => m > 0) >= 3).ToList();
+
+using (StreamWriter sw = new(@"..\..\..\joltejelok.txt", false, System.Text.Encoding.UTF8))
+{
+    foreach (var cow in eligibleCows)
+    {
+        sw.WriteLine($"{cow.Id} {cow.HetiAtlag}");
+    }
+}
+
+Console.WriteLine($"6. Feladat:\n {eligibleCows.Count} darab sort írtam az állományba.");
+
+Console.Write("7. Feladat: Kérem egy tehén azonosítóját:\n ");
+string? keresettId = Console.ReadLine();
+
+int leszarmazottak = happyCows.Count(t => t.Id.StartsWith(keresettId) && t.Id.Length > keresettId.Length);
+
+Console.WriteLine($"A leszármazottak száma: {leszarmazottak}");
+
+Console.ReadKey();
---
+++ tehenek/Tehen.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehenek
+{
+    class Tehen : IEquatable<Tehen>
+    {
+
+        public string Id { get; private set; }
+        public int[] Mennyisegek { get; private set; }
+
+        public bool Equals(Tehen masik)
+        {
+            return masik != null && masik.Id == this.Id;
+        }
+
+        public void EredmenytRogzit(string nap, string menyiseg)
+        {
+            Mennyisegek[int.Parse(nap)] = int.Parse(menyiseg);
+        }
+
+        public int HetiTej
+        {
+            get
+            {
+                return Mennyisegek.Sum();
+            }
+        }
+
+        public int HetiAtlag
+        {
+            get
+            {
+                var validDays = Mennyisegek.Where(m => m > 0).ToArray();
+                if (validDays.Length < 3)
+                    return -1;
+                return (int)Math.Round(validDays.Average());
+            }
+        }
+
+        public Tehen(string id)
+        {
+            Id = id;
+            Mennyisegek = new int[7];
+        }
+    }
+}
